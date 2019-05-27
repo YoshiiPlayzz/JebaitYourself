282b40531cf1283038d09ceffa10e738f3eb5ddc
Joshua Hatzinger <joshuahatzinger@outlook.de>
2019-05-27 16:55:13 +0200

    Added Countdown Frame

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 
+import javax.swing.*;
 import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -11,23 +12,84 @@ public class Main {
 
 
     private static double trys;
+    static JFrame frame;
 
     public static void main(String[] args)
     {
 
+
+
+        frame = new JFrame("An Usefull Program");
+
+        frame.setSize(200, 200);
+        frame.addNotify();
+        Dimension d = new Dimension();
+        d.setSize(200, 200);
+        Dimension x = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setBounds(((int)x.getWidth() / 2)  - 200, ((int)x.getHeight() / 2) - 200, 200, 200);
+
+        frame.setMinimumSize(d);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setAlwaysOnTop(true);
+        JTextField txt = new JTextField();
+        txt.setBounds(100, 100, 50, 50);
+        new Thread() {
+            int counter = 10;
+            public void run() {
+                while(counter >= 0) {
+                    txt.setText("" +  (counter--));
+                    try{
+                        Thread.sleep(1000);
+                    } catch(Exception e) {}
+                }
+            }
+        }.start();
+        frame.add(txt);
+
+
+
+        frame.pack();
+
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        frame.dispose();
         for(int i = 0; i < 2; i++){
             try {
                 i--;
                 trys++;
-                System.out.println("You got " + (int)trys + " times jebaited");
+
                 openWebpage(new URL("https://www.youtube.com/embed/tEo9sqzX1lQ?autoplay=1"));
-                Thread.sleep(1700);
+                long delay = (int)(Math.random() * 5000 )+1700;
+                System.out.println("You got " + (int)trys + " times jebaited: " + delay + "ms");
+                Thread.sleep( delay);
+
             } catch (MalformedURLException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
 
+    }
+
+
+    private void initialize() {
+
+
+    }
+    public static void paint(Graphics g, String draw, String font, int fontsize, int fonttype) {
+        Dimension d = frame.getPreferredSize();
+
+
+        g.setFont(new Font(font,fonttype, fontsize));
+
+        g.setColor(Color.red);
+
+        g.drawString(draw, 10, 20);
     }
 
     public static boolean openWebpage(URI uri) {
